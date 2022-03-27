e184d036eb32c3f7c1c5531715d196e83ba9a3e0
Aljoscha Zynga <aljoscha.zynga@gmail.com>
2022-03-27 22:16:33 +0200

    create grid from solid line

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+canvas.style.backgroundColor = 'hsl(210, 50%, 15%)';
 const gridColor = 'hsl(210, 50%, 90%)';
-
-// ctx.fillStyle = drawColor;
-// ctx.strokeStyle = drawColor;
 
 
 const getRelationOfPoints = (origin, otherPoint) => {
@@ -26,12 +24,13 @@ const getRelationOfPoints = (origin, otherPoint) => {
     return relation;
 }
 
+
 const getShiftedPixel = (origin, attractor, multiplier = 1) => {
     const { distance, angle } = getRelationOfPoints(origin, attractor);
 
     const shift = {
-        x: Math.cos(angle) * (multiplier * 10000 / distance ** 2),
-        y: Math.sin(angle) * (multiplier * 10000 / distance ** 2)
+        x: Math.cos(angle) * (multiplier * 1000000 / distance ** 2),
+        y: Math.sin(angle) * (multiplier * 1000000 / distance ** 2)
     };
 
     const shiftDistance = Math.sqrt(shift.x ** 2 + shift.y ** 2);
@@ -51,40 +50,48 @@ const getShiftedPixel = (origin, attractor, multiplier = 1) => {
 const drawGrid = (canvas, gridSize = 100, pixelDistance = 1, attractor, multiplier = 100) => {
     
     const drawLine = (canvas, direction, position, attractor, multiplier) => {
-
-        const length = direction === 'vertical' ? canvas.height : canvas.width;
-    
+        const isVertical = direction === 'vertical';
+        const length = isVertical ? canvas.height : canvas.width;
+        
+        ctx.beginPath();
+        
         for(let pixelNumber = 0; pixelNumber <= length; pixelNumber += pixelDistance) {
-
+            
             const relativePosition = {
-                x: direction === 'vertical' ? position : pixelNumber,
-                y: direction === 'vertical' ? pixelNumber : position
+                x: isVertical ? position : pixelNumber,
+                y: isVertical ? pixelNumber : position
             }
             
             const { shiftedX, shiftedY } = getShiftedPixel(relativePosition, attractor, multiplier)
-
-            ctx.fillRect(
-                shiftedX,
-                shiftedY,
-                2,
-                2);
+            
+            if(pixelNumber === 0) {
+                ctx.moveTo(shiftedX, shiftedY);
+            } else if (pixelNumber + pixelDistance > length) {
+                const pixelsToEnd = length - pixelNumber;
+                if(isVertical) {
+                    ctx.lineTo(shiftedX, shiftedY + pixelsToEnd);
+                } else {
+                    ctx.lineTo(shiftedX + pixelsToEnd, shiftedY);
+                }
+            } else {
+                ctx.lineTo(shiftedX, shiftedY);
+            };
         };
-    }
+        
+        ctx.stroke();
+        ctx.closePath();
+    };
     
     for(let i = 0; i <= canvas.height; i += gridSize) {
         drawLine(canvas, 'horizontal', i, attractor, multiplier);
     };
     drawLine(canvas, 'horizontal', canvas.height, attractor, multiplier);
-    
+
     for(let i = 0; i <= canvas.width; i += gridSize) {
         drawLine(canvas, 'vertical', i, attractor, multiplier);
     };
     drawLine(canvas, 'vertical', canvas.width, attractor, multiplier);
-
 };
-
-ctx.fillStyle = gridColor;
-drawGrid(canvas, 60, 10, {x: 0, y: 0}, 0);
 
 
 canvas.onmousemove = canvas.onmousedown = canvas.onmouseup = canvas.ontouchmove = canvas.ontouchstart = canvas.ontouchend = (event) => {
@@ -92,20 +99,31 @@ canvas.onmousemove = canvas.onmousedown = canvas.onmouseup = canvas.ontouchmove 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         const mousePosition = {
-            x: event.offsetX,
-            y: event.offsetY
+            x: event.pageX,
+            y: event.pageY
         };
         
-        
         const mousedown = event.buttons === 1;
-        
-        const multiplier = mousedown ? 1000 : 100;
+        const multiplier = mousedown ? 10 : 1;
     
-        ctx.fillStyle = gridColor;
-        drawGrid(canvas, 60, 10, mousePosition, multiplier);
+        ctx.strokeStyle = gridColor;
+        ctx.lineWidth = 0.5;
+        drawGrid(canvas, canvas.width / 40, 10, mousePosition, multiplier);
     
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = canvas.style.backgroundColor;
         ctx.beginPath();
+        ctx.arc(
+            mousePosition.x,
+            mousePosition.y,
+            22,
+            0,
+            2 * Math.PI
+        );
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.fillStyle = 'black';
         ctx.arc(
             mousePosition.x,
             mousePosition.y,
@@ -114,5 +132,6 @@ canvas.onmousemove = canvas.onmousedown = canvas.onmouseup = canvas.ontouchmove 
             2 * Math.PI
         );
         ctx.fill();
+        ctx.closePath();
     });
 };
